9735220bd9df32101c65336d1ce7bcd8556105a7
vishnumoger <vishnumoger@gmail.com>
2023-08-22 20:59:54 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+// Create a TCP server
+const server = net.createServer(socket => {
+    console.log('Device connected:', socket.remoteAddress, socket.remotePort);
+
+    // Set up event listeners for socket
+    socket.on('data', data => {
+        console.log('Received data:', data.toString());
+        // Process the data from the IoT device as needed
+    });
+
+    socket.on('end', () => {
+        console.log('Device disconnected:', socket.remoteAddress, socket.remotePort);
+    });
+
+    socket.on('error', err => {
+        console.error('Socket error:', err);
+    });
+});
+
+const PORT = 9001; // You can change this to your desired port number
+
+// Start the server and listen on the specified port
+server.listen(PORT, () => {
+    console.log('Server listening on port', PORT);
+});
