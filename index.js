ca93cf970a396f96feaf0b83789d6c19abebb918
vishnumoger <vishnumoger@gmail.com>
2023-08-26 20:51:07 +0530

    tcp server

--- index.js
+++ index.js
@@ -76,31 +76,6 @@ server.listen(9001, () => {
   console.log('TCP server is listening on port 9001');
 });
 
-const cors = require('cors');
-const app = express();
-app.use(cors())
-app.use(express.json());
-
-app.get('/api/startCharging/CHARGEON', async (req, res, next) => {
-  console.log(1111)
-  Status = true;
-  console.log(Status)
-  res.send("CHARGE ON")
-});
-
-app.get('/api/startCharging/CHARGEOFF', async (req, res, next) => {
-  console.log(1111)
-  Status = false;
-  console.log(Status)
-  res.send("CHARGE OFF")
-});
-
-
-app.listen(9002, () => {
-  console.log('API server is listening on port 9002')
-})
-
-
 async function updateIOTStatus(iotId) {
   const client = new MongoClient(mongoUrl, { useUnifiedTopology: true });
 
